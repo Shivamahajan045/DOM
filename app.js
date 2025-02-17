8fd1d1fc1dc67d1c1d7eed5949f55bf7c6006648
shivakumar <shivamahajan055@gmail.com>
2025-02-17 12:42:27 +0530

    added Creating Elements & DOM Relations

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
-// Write your code below:
-const heading = document.getElementById("main-heading");
-heading.textContent = "Fruit World";
-heading.style.color = "orange";
+let mainHeading = document.querySelector("#main-heading");
+let subHeading = document.createElement("h3");
+subHeading.textContent = "Buy high quality organic fruits online";
+mainHeading.appendChild(subHeading);
+subHeading.style.fontStyle = "italic";
 
-const header = document.getElementById("header");
-header.style.background = "green";
-header.style.borderBottom = "orange";
+let para = document.createElement("p");
+para.textContent = "Total fruits: 4";
+para.setAttribute("id", "fruits-total");
 
-const basketHeading = document.getElementById("basket-heading");
-basketHeading.style.color = "green";
-
-const div = document.getElementById("thanks");
-
-div.innerHTML = "<p>Please visit us again</p>";
+let ul = document.querySelector(".fruits");
+ul.prepend(para);
